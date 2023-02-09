8cb5cb67db128753c7f886adff94b29acc2cbcdf
Zaynn-Lea <gely.leandre@gmail.com>
2023-02-09 15:48:28 +0100

    creating a separate repository for my graphic_tool package, complete with a test and presentation file (main.py)

---
+++ main.py
@@ -0,0 +1,19 @@
+
+"""
+TODO : this file (doc + testing)
+
+TODO : go through every docstring to update it with the spaces and the " --------- "
+
+TODO : update the test for type, class and subclass
+
+TODO : setup the github
+"""
+
+
+def main(*args, **kwargs):
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
